34157340f681772bebf755983107d4a24df391b8
FatherAnarchy <70991592+GickiAnarchy@users.noreply.github.com>
2021-01-08 12:42:01 -0600

    2

--- 3_RUN_DAYZ _SIMPLE_MISSION.bat
+++ 3_RUN_DAYZ _SIMPLE_MISSION.bat
@@ -1,4 +1,4 @@
 cd C:\Program Files (x86)\Steam\steamapps\common\DayZ
-.\DayZDiag_x64.exe "-mod=!Workshop@CF;!Workshop@Community-Online-Tools;P:\@DOM_Grill;" -mission=P:\DayZOffline.Chernarus -filePatching
+.\DayZDiag_x64.exe "-mod=!Workshop\@CF;!Workshop\@Community-Online-Tools;P:\@DOM_Grill;" -mission=P:\DayZOffline.Chernarus -filePatching
 
 pause
