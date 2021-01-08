30a95681d5e944065a97bfd41730f5aee9c255ea
FatherAnarchy <70991592+GickiAnarchy@users.noreply.github.com>
2021-01-08 11:12:58 -0600

    Update init.c

--- DayZOffline.Chernarus/init.c
+++ DayZOffline.Chernarus/init.c
@@ -6,12 +6,12 @@ class CustomMission: MissionGameplay
 Mission CreateCustomMission(string path)
 {
 	return new CustomMission();
-}
+};
 
 void main()
 {
-	PlayerBase player = PlayerBase.Cast( (GetGame().CreatePlayer( NULL, "SurvivorF_Linda", 2200 10 2200, 0, "NONE") ) );
+	PlayerBase player = PlayerBase.Cast((GetGame().CreatePlayer(NULL, "SurvivorF_Linda", "2200 10 2200", 0, "NONE")));
 	player.CreateInInventory("TShirt_Black");
 	GetGame().SelectPlayer(NULL, player);
-}
+};
 
