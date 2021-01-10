0b0548b95cd7025defb26af8fa0525324e1487d6
FatherAnarchy <fatheranarchy@programmer.net>
2021-01-09 21:57:10 -0600

    Update 3_RUN_DAYZ.bat

--- 3_RUN_DAYZ.bat
+++ 3_RUN_DAYZ.bat
@@ -1,4 +1,2 @@
 cd C:\Program Files (x86)\Steam\steamapps\common\DayZ
 .\DayZDiag_x64.exe "-mod=P:\@DOM_Grill;C:\Program Files (x86)\Steam\steamapps\common\DayZ\!Workshop\@CF;" -mission=C:\Program Files (x86)\Steam\steamapps\common\DayZ\Missions\DayZCommunityOfflineMode.ChernarusPlus -filePatching
-
-exit
